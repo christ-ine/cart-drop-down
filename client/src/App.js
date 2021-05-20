ac8041386473d678de95ef018f08d5f71fa1d009
Christine Ross <christinejoyross@gmail.com>
2021-05-19 21:21:49 -0700

    app.jd edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Hero from './components/Hero.js/Hero'
-import Navbar from './components/Navbar/Navbar'
-import HeroSection from './components/HeroSection/HeroSection'
 import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Header /> */}
-        {/* <Hero /> */}
-        {/* <Navbar /> */}
-        {/* <HeroSection /> */}
         <HomeScreen />
       </Router>
     </div>
